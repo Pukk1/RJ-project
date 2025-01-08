96abab3a4538dd861035957a526c70db3632ac3f
mobest1an <erikk202@gmail.com>
2025-01-08 19:07:13 +0300

    add flowable

--- src/main/java/com/iver/rjproject/service/ChineseMakerService.java
+++ src/main/java/com/iver/rjproject/service/ChineseMakerService.java
@@ -1,20 +1,27 @@
 package com.iver.rjproject.service;
 
+import com.iver.rjproject.model.CPU;
+import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Stream;
 
+@Service
 public class ChineseMakerService implements MakerService {
 
     private final ThreadPoolExecutor executor;
+    private final Scheduler scheduler;
+    private final Flowable<CPU> cpuFlowable;
+    private final CpuGenerator cpuGenerator;
 
-    public ChineseMakerService() {
+    public ChineseMakerService(CpuGenerator cpuGenerator) {
+        this.cpuGenerator = cpuGenerator;
         this.executor = (ThreadPoolExecutor) Executors.newCachedThreadPool();
-
-        Scheduler scheduler = Schedulers.from(executor);
-
+        this.scheduler = Schedulers.from(executor);
+        cpuFlowable = Flowable.fromStream(Stream.generate(cpuGenerator::generate));
     }
 }
--- src/main/java/com/iver/rjproject/service/CpuGenerator.java
+++ src/main/java/com/iver/rjproject/service/CpuGenerator.java
@@ -1,8 +1,13 @@
 package com.iver.rjproject.service;
 
+import com.iver.rjproject.model.CPU;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CpuGenerator {
+public class CpuGenerator implements Generator<CPU> {
 
+    @Override
+    public CPU generate() {
+        return null;
+    }
 }
