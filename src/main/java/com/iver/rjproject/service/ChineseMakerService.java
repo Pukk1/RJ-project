f80c9de17a2a0dacd5691bc36b55b6c5d02abe22
mobest1an <erikk202@gmail.com>
2025-01-08 20:43:43 +0300

    add flowable

--- src/main/java/com/iver/rjproject/service/ChineseMakerService.java
+++ src/main/java/com/iver/rjproject/service/ChineseMakerService.java
@@ -3,13 +3,16 @@ package com.iver.rjproject.service;
 import com.iver.rjproject.model.DomainModel;
 import com.iver.rjproject.records.Computer;
 import com.iver.rjproject.util.CustomSubscriber;
+import io.reactivex.rxjava3.core.BackpressureStrategy;
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Scheduler;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Stream;
 
 @Service
 public class ChineseMakerService implements MakerService {
@@ -28,7 +31,7 @@ public class ChineseMakerService implements MakerService {
         });
 //        flowable = Observable.fromStream(Stream.generate(generator::generate)).toFlowable(BackpressureStrategy.BUFFER);
         flowable
-                .observeOn(scheduler)
+                .subscribeOn(scheduler)
                 .subscribe(customSubscriber);
 
         executor.setCorePoolSize(domainModel.getMakersCount());
